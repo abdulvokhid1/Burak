7c7b8813b8bb02ad479b9b907000990a9a2328b0
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2024-02-29 17:03:27 +0900

    feat: develop member scheme model

---
+++ src/scheme/Member.model.ts
@@ -0,0 +1,59 @@
+import mongoose,{Schema} from "mongoose";
+
+import { MemberType, MemberStatus } from "../libs/enums/member.enum";
+
+
+const memberSchema = new Schema({
+    memberType: {
+        type: String,
+        enum: MemberType,
+        default: MemberType.USER,
+    },
+    
+    memberStatus : {
+        type: String,
+        enum: MemberStatus,
+        default: MemberStatus.ACTIVE
+    },
+
+    memberNick:{
+        type: String,
+        index: {unique:true, sparse:true},
+        required:true,
+    },
+
+    memberPhone:{
+        type: String,
+        index: {unique:true, sparse:true},
+        required:true,
+    },
+
+    memberPassword : {
+        type:String,
+        select:false,
+        required: true
+    },
+
+    memberAddress: {
+        type: String
+    },
+
+    memberDesk: {
+        type: String
+    },
+
+    memberImage: {
+        type: String
+    },
+
+    memberPoints: {
+        type : Number,
+        default:0
+    },
+
+
+},{timestamps:true} //updatedAt, createdAt
+)
+
+
+export default mongoose.model("Member",memberSchema)
--- src/train.ts
+++ src/train.ts
@@ -5,3 +5,12 @@ shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul 
 MASALAN: getPositive([1, -4, 2]) return qiladi "12" 
   */
 
+function getPositive(arr: number[]) {
+  const positive: string[] = arr
+    .filter((num) => Number.isInteger(num) && num > 0)
+    .map((num) => num.toString());
+
+  const result: string = positive.join("");
+  return result;
+}
+console.log(getPositive([1, -4, 2]));
