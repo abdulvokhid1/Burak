141f51b5602c13cc29183dc8a0c14d1fc4487b09
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2024-03-02 04:28:48 +0900

    feat: create restaurant member related to get and post endpoints

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
-import app from "./app";
-
+import app from "./app"
 
 
 mongoose.connect(process.env.MONGO_URL as string)
