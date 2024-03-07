12c3caecdeaeb5e4c0671df3b5d1664421fb96b2
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2024-03-07 18:51:53 +0900

    fix: modify restaurant login and signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,7 +2,7 @@ import MemberModel from "../scheme/Member.model";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 import Errors, { HttpCode, Message } from "../libs/Errors";
-
+import * as bcrypt from "bcryptjs"
 class MemberService {
     private readonly memberModel;
     constructor(){
@@ -14,6 +14,15 @@ class MemberService {
         .findOne({memberType: MemberType.RESTAURANT}).exec();
 
         if(exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+
+        console.log(input.memberPassword)
+        const salt = await bcrypt.genSalt();
+        input.memberPassword = await bcrypt.hash(input.memberPassword,salt)
+        console.log(input.memberPassword)
+
+
+
 
         try{
             const result =  await this.memberModel.create(input);
@@ -32,7 +41,8 @@ class MemberService {
 
         if(!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK)
 
-        const isMatch = input.memberPassword === member.memberPassword;
+        const isMatch = await bcrypt.compare(input.memberPassword, member.memberPassword)
+        // const isMatch = input.memberPassword === member.memberPassword;
 
         if(!isMatch){
             throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
--- src/train.ts
+++ src/train.ts
@@ -1,47 +1,12 @@
-/*
-H-TASK: 
+// I-TASK:
 
-shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
-MASALAN: getPositive([1, -4, 2]) return qiladi "12" 
-  */
-
-function getPositive(arr: number[]) {
-  const positive: string[] = arr
-    .filter((num) => Number.isInteger(num) && num > 0)
-    .map((num) => num.toString());
-
-  const result: string = positive.join("");
-  return result;
+function getDigits(str: string) {
+  let digits: string = "";
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(parseInt(str[i]))) {
+      digits += str[i];
+    }
+  }
+  return digits;
 }
-console.log(getPositive([1, -4, 2]));
-
-/*
-Project Standarts:
--Logging Standarts
--Naming Standarts :
-  function, method, variable => CAMEL;
-  class => PASCAL;
-  folder, file => KEBAB;
-  css => SNAKE;
--Error Handling
-    
-*/
- 
-/* API => Application Programming Interface:
-
-Traditional Api
-Rest Api
-GrapQL Api
-*/
-
-
-/* I-TASK: 
-
-Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
-MASALAN: getDigits("m14i1t") return qiladi "141" */
-
-
-function getDigits(){
-
-}
-getDigits() 
+console.log(getDigits("m14i1t"));
