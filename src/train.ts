28b4aee1150989d6cd95decd1c52cc493225e123
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2024-03-10 14:02:49 +0900

    feat: J task is done

--- src/train.ts
+++ src/train.ts
@@ -1,12 +1,17 @@
-// I-TASK:
+// J-TASK:
 
-function getDigits(str: string) {
-  let digits: string = "";
-  for (let i = 0; i < str.length; i++) {
-    if (!isNaN(parseInt(str[i]))) {
-      digits += str[i];
+function findLongestWord(input: string) {
+  let words: string[] = input.split(" ");
+
+  let longWord: string = "";
+  let maxLength: number = 0;
+
+  for (let word of words) {
+    if (word.length > maxLength) {
+      longWord = word;
+      maxLength = word.length;
     }
   }
-  return digits;
+  return longWord;
 }
-console.log(getDigits("m14i1t"));
+console.log(findLongestWord("I come from Uzbekistan"));
