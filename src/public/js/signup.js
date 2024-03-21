e462ee880aaa54c2c04e5c0d20a7e62590ae2c2d
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2024-03-21 14:58:02 +0900

    feat: signup image  validation process

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,30 @@
 console.log("Signup frontend javascript file");
 
 $(function(){
+    $(function () {
+        const fileTarget = $(".file-box .upload-hidden");
+        let filename;
+      
+        fileTarget.on("change", function () {
+          if (window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
+            const fileType = uploadFile["type"];
+            const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+            if (!validImageType.includes(fileType)) {
+              alert("Please insert only jpeg, jpg, png!");
+            } else {
+              if (uploadFile) {
+                console.log(URL.createObjectURL(uploadFile));
+                $(".upload-img-frame")
+                  .attr("src", URL.createObjectURL(uploadFile))
+                  .addClass("success");
+              }
+              filename = $(this)[0].files[0].name;
+            }
+            $(this).siblings(".upload-name").val(filename);
+          }
+        });
+      });
   
   });
 
@@ -19,4 +43,12 @@ $(function(){
         alert("password not match, please check again");
         return false
     }
+
+    const memberImage = $(".member-image").get(0).files[0]
+    ? $(".member-image").get(0).files[0].name
+    : null;
+  if (!memberImage) {
+    alert("Please insert restaurant image!");
+    return false;
+  }
   }
