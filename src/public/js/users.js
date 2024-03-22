9d1ff81524ee0057deb9e8f6ff6b6b1862565e3a
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2024-03-23 05:21:43 +0900

    feat: frontend chosen user update logic develop

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,29 @@
+
 console.log("Users frontend javascript file");
+$(function() {
+    $(".member-status").on("change",  function(e) {
+        const id  = e.target.id;
+        const memberStatus = $(`#${id}.member-status`).val();
+        console.log("id", id);
+        console.log("memberStatus", memberStatus);
+
+         axios.post("/admin/user/edit", {
+            _id: id,
+            memberStatus: memberStatus,
+            }).
+            then((response) =>{
+                console.log("response", response)
+                const result = response.data;
+                console.log("result", result)
+
+                if( result.data) {
+                    console.log("User Uploaded");
+                    $(".member-status").blur();
+                } else alert("User update failed!");
+            }).
+        catch((err)=>{
+            console.log(err);
+            alert("Product update failed!");
+        })
+    });
+});
