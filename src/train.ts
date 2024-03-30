c6a4d5c5f9beb1d13831ba9fd9f3833de66eecdc
ali <ibrakhimov7747a@gmail.com>
2024-03-30 18:09:35 +0900

    feat : task s done

--- src/train.ts
+++ src/train.ts
@@ -1,30 +1,13 @@
-// TASK-Q
+// TASK-S
 
-
-
-function hasProperty(a: { [key: string]: any }, b: string) {
-  for (let key in a) {
-    if (key === b) {
-      return true;
-    }
-  }
-  return false;
-} 
-
-console.log(hasProperty({ name: "BMW", model: "M3" }, "model"));
-
-
-// TASK-R
-
-function calculate(str: string): number {
-  const numbers = str.split("+");
-
-  const result = numbers.reduce((acc, num) => acc + parseInt(num), 0);
-
-  return result;
+function missingNumber(arr: number[]): number {
+  const n: number = arr.length;
+  const numbers: number = (n * (n + 1)) / 2;
+  const arrayNum: number = arr.reduce((num, num2) => num + num2, 0);
+  return numbers - arrayNum;
 }
 
-console.log(calculate("1+3"));
+console.log(missingNumber([3, 0, 1]));
 
 
 
